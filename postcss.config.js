3bc93e09319aaae142d1b48c7296fb861873f8d2
Alexander Sohn <github@asohn.de>
2022-10-10 23:17:36 +0200

    Fix navigation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./hugo_stats.json'],
-  safelist: ['theme--dark', 'theme--light', 'icon'],
+  safelist: ['theme--dark', 'theme--light', 'nav--active'],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
     return els.tags.concat(els.classes, els.ids);
